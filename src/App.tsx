4caccff9dae2213039be3cb059331ef8ae874391
shanecranor <shanecranor@gmail.com>
2023-06-02 19:51:51 -0500

    fixed build issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,6 @@
-import { useState } from 'react'
-
 import './App.css'
 import Draggable from './Draggable'
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
     <Draggable defaultPosition={{x:0,y:0}}>
