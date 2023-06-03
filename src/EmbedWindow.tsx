55c3ecdccdd38a2e6627aefc3cd130729d9052be
shanecranor <shanecranor@gmail.com>
2023-06-03 12:56:12 -0500

    fixed weird bug

--- src/EmbedWindow.tsx
+++ src/EmbedWindow.tsx
@@ -7,47 +7,51 @@ import { EmbedInfo } from "./types";
 //TODO: background windows should be differentiated somehow
 export default function EmbedWindow(
 	{
-		embedInfo: { 
-			id, 
+		embedInfo: {
+			id,
 			iframeSrc,
 			title,
 			tooltipDescription,
 			dimensions,
-			isResizeable 
+			isResizeable
 		},
 		isFocused,
 	}: { embedInfo: EmbedInfo, isFocused: boolean }) {
 	const [isDragging, setIsDragging] = useState<boolean>(false);
-	if(isResizeable){ //todo add resizeable functionality
+	if (isResizeable) { //todo add resizeable functionality
 		console.log("resizeable");
 		console.log(id, tooltipDescription)
 	}
 	// const [isResizing, setIsResizing] = useState<boolean>(false);
 	return <Draggable defaultPosition={{ x: 0, y: 0 }}>
-		<div
-			className="title-bar"
+		<div className="addon-window"
 			onMouseDown={() => { setIsDragging(true); }}
 			onMouseUp={() => setIsDragging(false)}
-			style={{
-				background: "black",
-				color: "white",
-				width: `${dimensions.x}px`,
-				padding: "10px",
-				boxSizing: "border-box",
-			}}
 		>
-			{title}
+			<div
+				className="title-bar"
+				style={{
+					background: "black",
+					color: "white",
+					width: `${dimensions.x}px`,
+					padding: "10px",
+					boxSizing: "border-box",
+				}}
+			>
+				{title}
+			</div>
+			
+			<iframe
+				src={iframeSrc}
+				style={{
+					border: "1px solid black",
+					boxSizing: "border-box",
+					width: "100%",
+					height: `${dimensions.y}px`,
+					pointerEvents: isDragging || !isFocused ? "none" : "inherit",
+				}}
+			></iframe>
 		</div>
-		<iframe
-			src={iframeSrc}
-			style={{
-				border: "1px solid black",
-				boxSizing: "border-box",
-				width: "100%",
-				height: `${dimensions.y}px`,
-				pointerEvents: isDragging || !isFocused ? "none" : "inherit",
-			}}
-		></iframe>
 	</Draggable>
 
 
