579c964c07a42d2017f1e1ccdd6f68aee315c9fe
shanecranor <shanecranor@gmail.com>
2023-06-02 19:42:52 -0500

    added iframe to test

--- src/App.tsx
+++ src/App.tsx
@@ -8,9 +8,10 @@ function App() {
   return (
     <>
     <Draggable defaultPosition={{x:0,y:0}}>
-      <div style={{background:"red"}}>
-        test :)
+      <div style={{background:"black", color:"white", width: "900px", padding: "10px", boxSizing:"border-box"}}>
+      🐷 Pigtionary Addon 
       </div>
+      <iframe src="https://pigtionary-embed.netlify.app/streamer.html" style={{border:"none", boxSizing:"border-box", width:"100%", height:"400px"}}></iframe>
     </Draggable>
     </>
     
