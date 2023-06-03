b87dc50daae36c481900da51f7b586ef24adeb0e
shanecranor <shanecranor@gmail.com>
2023-06-02 21:57:01 -0500

    fixed chrome dragging bug

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
 import './App.css'
+import { useState } from 'react'
 import Draggable from './Draggable'
 function App() {
+  const [isDragging, setIsDragging] = useState<boolean>(false)
   return (
     <>
     <Draggable defaultPosition={{x:0,y:0}}>
-      <div style={{background:"black", color:"white", width: "900px", padding: "10px", boxSizing:"border-box"}}>
+      <div 
+      onMouseDown={()=>setIsDragging(true)}
+      onMouseUp={()=>setIsDragging(false)}
+      style={{background:"black", color:"white", width: "900px", padding: "10px", boxSizing:"border-box"}}
+      >
       🐷 Pigtionary Addon 
       </div>
-      <iframe src="https://pigtionary-embed.netlify.app/streamer.html" style={{border:"none", boxSizing:"border-box", width:"100%", height:"400px"}}></iframe>
+      <iframe src="https://pigtionary-embed.netlify.app/streamer.html" 
+      style={{border:"none", boxSizing:"border-box", width:"100%", height:"400px", pointerEvents: isDragging ? "none" : "inherit"}}
+      ></iframe>
     </Draggable>
     </>
     
